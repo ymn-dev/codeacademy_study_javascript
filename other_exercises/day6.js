dcb44898251573e7db49b1047f55e0827da4000f
ymn-dev <sornniyom.a@gmail.com>
2023-09-12 11:43:17 +0700

    added get weekly expense

--- other_exercises/day6.js
+++ other_exercises/day6.js
@@ -10,8 +10,7 @@ const myExpense = {
   log: [],
   addLog(date, category, moneyspent) {
     category = category.toLowerCase();
-    const dateSplit = date.split("-"); // Split the string into parts, YYYY,MM,DD
-    const myDate = new Date(Number(dateSplit[0]), Number(dateSplit[1]) - 1, Number(dateSplit[2]));
+    const myDate = this.convertStringToDateObject(date);
     const existingDay = this.log.find((day) => day.date.toDateString() === myDate.toDateString());
 
     if (existingDay) {
@@ -28,16 +27,41 @@ const myExpense = {
       this.log.push(newDay);
     }
   },
-  printWeeklyExpense(week) {},
-  printMonthlyExpense(month) {},
+  convertStringToDateObject(dateString) {
+    const dateSplit = dateString.split("-"); // Split the string into parts, YYYY,MM,DD
+    const myDate = new Date(Number(dateSplit[0]), Number(dateSplit[1]) - 1, Number(dateSplit[2]));
+    return myDate;
+  },
+  getWeeklyExpense(date) {
+    //will take input of 7 days before it
+    let myDate = this.convertStringToDateObject(date);
+    let weeklyExpense = {};
+    for (let i = 0; i < 7; i++) {
+      myDate.setDate(myDate.getDate() - 1);
+      const existingDay = this.log.find((day) => day.date.toDateString() === myDate.toDateString());
+      if (existingDay) {
+        for (const key in existingDay) {
+          if (key !== "date") {
+            if (!weeklyExpense[key]) {
+              weeklyExpense[key] = existingDay[key];
+            } else {
+              weeklyExpense[key] += existingDay[key];
+            }
+          }
+        }
+      } else continue;
+    }
+    return weeklyExpense;
+  },
+  // getMonthlyExpense(year, month) {},
 };
 
 myExpense.addLog("2023-9-12", "entertainment", 1500);
 myExpense.addLog("2023-9-12", "food", 400);
 
 myExpense.addLog("2023-9-11", "food", 500);
-console.log(myExpense.log);
-
+myExpense.addLog("2023-9-10", "transport", 500);
+console.log(myExpense.getWeeklyExpense("2023-9-13"));
 /*
 [Assignment 6 - 20230912]
 - Assume you are going to the supermarket, and you pick many items and put to the basket
