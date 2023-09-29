701a8da0004df5a404de685d0210a93a2b5def07
ymn-dev <sornniyom.a@gmail.com>
2023-09-29 22:07:26 +0700

    putting unique id for each quote

--- quote-api/server.js
+++ quote-api/server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 
 const { quotes } = require("./data");
+quotes.forEach((quote, index) => (quote.id = index + 1));
 const { getRandomElement } = require("./utils");
-
 const PORT = process.env.PORT || 4001;
-
 app.use(express.static("public"));
 
 app.get("/api/quotes/random", (req, res, next) => {
@@ -27,7 +26,7 @@ app.post("/api/quotes", (req, res, next) => {
   if (!quote || !person) {
     res.status(404).send("not a valid request");
   } else {
-    const newQuote = { quote, person };
+    const newQuote = { id: quotes.length + 1, quote, person };
     quotes.push(newQuote);
     res.send({
       quote: newQuote,
