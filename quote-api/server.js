d4a67ab406cf1b7e246fcbca11815940b20d2521
ymn-dev <sornniyom.a@gmail.com>
2023-09-29 21:55:19 +0700

    step 4 done

--- quote-api/server.js
+++ quote-api/server.js
@@ -1,10 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/api/quotes/random", (req, res, next) => {
+  const quote = getRandomElement(quotes);
+  // console.log({ quote });
+  res.json({
+    quote,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`the server is running on port ${PORT}`);
+});
