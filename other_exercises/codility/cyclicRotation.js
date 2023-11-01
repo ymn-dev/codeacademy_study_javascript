ea7833153781aecbabe112af4d750fae746205dd
ymn-dev <sornniyom.a@gmail.com>
2023-11-01 22:02:09 +0700

    cyclicRotation done

---
+++ other_exercises/codility/cyclicRotation.js
@@ -0,0 +1,12 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A, K) {
+  // Implement your solution here
+  if (K === A.length || A.length === 0) return A;
+  for (let i = 0; i < K % A.length; i++) {
+    const data = A.pop();
+    A.unshift(data);
+  }
+  return A;
+}
