0845025d95812a462393a410d527e1bea4368aaa
ymn-dev <sornniyom.a@gmail.com>
2023-11-09 16:29:36 +0700

    PermCheck O(n) done

---
+++ other_exercises/codility/PermCheck.ts
@@ -0,0 +1,12 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function PermCheck(A: number[]): number {
+  // Implement your solution here
+  const checker: boolean[] = Array(A.length).fill(false);
+  for (let i: number = 0; i < A.length; i++) {
+    if (checker[A[i] - 1] || A[i] > A.length) return 0;
+    if (!checker[A[i] - 1]) checker[A[i] - 1] = true;
+  }
+  return 1;
+}
