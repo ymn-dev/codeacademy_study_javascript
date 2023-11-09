4af3a9380f80ae98e393b40aec86c96272f8eb65
ymn-dev <sornniyom.a@gmail.com>
2023-11-09 16:09:43 +0700

    FrogRiverOne o(n) done

---
+++ other_exercises/codility/FrogRiverOne.ts
@@ -0,0 +1,26 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function FrogRiverOne(X: number, A: number[]): number {
+  // Implement your solution here
+  let earliestTime: number = -1;
+  interface leafPosition {
+    [position: number]: boolean;
+  }
+  let count: number = 0;
+  const leavesPositions: leafPosition = {};
+  if (A.length < X) return -1;
+  for (let i: number = 0; i < A.length; i++) {
+    if (A[i] in leavesPositions) {
+      continue;
+    } else {
+      leavesPositions[A[i]] = true;
+      count++;
+    }
+    if (count === X) {
+      earliestTime = i;
+      break;
+    }
+  }
+  return earliestTime;
+}
