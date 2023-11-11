14edbb5344f146012080f409b3bf0c5a0ede8195
ymn-dev <sornniyom.a@gmail.com>
2023-11-11 21:05:11 +0700

    maxcounter o(n+m) done

--- other_exercises/codility/MaxCounters.ts
+++ other_exercises/codility/MaxCounters.ts
@@ -4,15 +4,27 @@
 function MaxCounters(N: number, A: number[]): number[] {
   // Implement your solution here
   const maxCounter = N + 1;
+  let previousMaxUpdate: number = 0;
   let highestCount: number = 0;
   let resultArray: number[] = Array(N).fill(0);
   for (let i: number = 0; i < A.length; i++) {
     if (A[i] === maxCounter) {
-      resultArray = Array(N).fill(highestCount);
+      previousMaxUpdate = highestCount;
     } else {
+      //check if the value havent updated before increase
+      if (resultArray[A[i] - 1] < previousMaxUpdate) {
+        resultArray[A[i] - 1] = previousMaxUpdate;
+      }
       resultArray[A[i] - 1]++;
       if (resultArray[A[i] - 1] > highestCount) highestCount = resultArray[A[i] - 1];
     }
   }
+  //update value that didnt get call later
+  for (let i: number = 0; i < N; i++) {
+    if (resultArray[i] < previousMaxUpdate) {
+      resultArray[i] = previousMaxUpdate;
+    }
+  }
+
   return resultArray;
 }
