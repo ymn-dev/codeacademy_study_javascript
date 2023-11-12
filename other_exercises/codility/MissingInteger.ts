621c3489491795722e106ddad7d7141c93175050
ymn-dev <sornniyom.a@gmail.com>
2023-11-12 20:09:02 +0700

    missing integer O(n)/O(NlogN) done

---
+++ other_exercises/codility/MissingInteger.ts
@@ -0,0 +1,23 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function MissingInteger(A: number[]): number {
+  // Implement your solution here
+  let min = 1;
+  interface Checker {
+    [myNum: number]: boolean;
+  }
+  let checker: Checker = {};
+  for (let i: number = 0; i < A.length; i++) {
+    if (A[i] < 1) continue;
+    if (checker[A[i]]) {
+      continue;
+    } else {
+      checker[A[i]] = true;
+    }
+  }
+  while (checker[min] === true) {
+    min++;
+  }
+  return min;
+}
