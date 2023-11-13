af3e052b075a86b27b3ef1d1895c31efece8e651
ymn-dev <sornniyom.a@gmail.com>
2023-11-13 21:48:50 +0700

    PassingCars O(n) done

---
+++ other_exercises/codility/PassingCars.ts
@@ -0,0 +1,17 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function PassingCars(A: number[]): number {
+  // Implement your solution here
+  let passingPair: number = 0;
+  let goWest: number = 0;
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] == 0) {
+      goWest++;
+    }
+    if (A[i] == 1) {
+      passingPair += goWest;
+    }
+  }
+  return passingPair > 1000000000 ? -1 : passingPair;
+}
