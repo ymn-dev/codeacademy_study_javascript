3044a2ee411a8e159893e0be94cf8c941c52fd7b
ymn-dev <sornniyom.a@gmail.com>
2023-11-15 14:49:11 +0700

    optimizing CountDiv

--- other_exercises/codility/CountDiv.ts
+++ other_exercises/codility/CountDiv.ts
@@ -3,18 +3,8 @@
 
 function CountDiv(A: number, B: number, K: number): number {
   // Implement your solution here
-  let divisibleCount: number = 0;
-  let startValue: number = A;
-  let startValueFound: boolean = false;
-  let endValue: number = B;
-  let endValueFound: boolean = false;
-  for (let i = 0; i < K; i++) {
-    if (startValue % K === 0) startValueFound = true;
-    if (!startValueFound) startValue++;
-    if (endValue % K === 0) endValueFound = true;
-    if (!endValueFound) endValue--;
-    if (startValueFound && endValueFound) break;
-  }
-  divisibleCount = endValue / K - startValue / K + 1;
-  return divisibleCount;
+  const maxDivisible: number = Math.floor(B / K);
+  const skipped: number = Math.floor(A / K);
+  //if the starting point mod K === 0 we need to +1
+  return A % K === 0 ? maxDivisible - skipped + 1 : maxDivisible - skipped;
 }
