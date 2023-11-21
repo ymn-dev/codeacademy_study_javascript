3dc43072bf85ef032bfe12d87f6d436464cd28fa
ymn-dev <sornniyom.a@gmail.com>
2023-11-21 16:01:57 +0700

    Distinct 100 done

---
+++ other_exercises/codility/Distinct.ts
@@ -0,0 +1,18 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function Distinct(A: number[]): number {
+  // Implement your solution here
+  let distinctCount = 0;
+  interface Checker {
+    [FoundNum: number]: boolean;
+  }
+  const checker: Checker = {};
+  for (let i = 0; i < A.length; i++) {
+    if (!(A[i] in checker)) {
+      checker[A[i]] = true;
+      distinctCount++;
+    }
+  }
+  return distinctCount;
+}
